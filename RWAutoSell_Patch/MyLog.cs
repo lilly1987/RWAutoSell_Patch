deb732bf1e94a18dff386766b989b5609e4b8d40
lilly <lilly1987@naver.com>
2025-09-24 09:35:08 +0900

    초기 커밋.

---
+++ RWAutoSell_Patch/MyLog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Lilly
+{
+    public static class MyLog
+    {
+        //public static void Warning(string text) => Log.Warning($"Lilly - {text}");
+        //public static void Error(string text) => Log.Error($"Lilly - {text}");
+        //public static void Message(string text) => Log.Message($"Lilly - {text}");
+
+        public static string MyText(string text, string memberName, int lineNumber, string className)
+        {
+            return $"<color=#FFC0FFFF>Lilly</color> <color=#00FF00FF>{className}</color>.<color=#FF8000FF>{memberName}</color> <color=#00FFFFFF>{lineNumber}</color> - {text}";
+        }
+
+
+        public static void Warning(string text,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            bool print=true
+            )
+        {
+            if (!print) return;
+            string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            Log.Warning(MyText(text, memberName, lineNumber, className));
+        }
+
+
+        public static void Error(string text,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            bool print = true)
+        {
+            if (!print) return;
+            string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            Log.Error(MyText(text, memberName, lineNumber, className));
+        }
+
+        public static void Message(string text,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0,
+            bool print = true)
+        {
+            if (!print) return;
+            string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
+            Log.Message(MyText(text, memberName, lineNumber, className));
+        }
+
+    }
+}
---
+++ RWAutoSell_Patch/RWAutoSell_Patch.cs
@@ -0,0 +1,178 @@
+﻿using HarmonyLib;
+using RWASFilterLib;
+using RWAutoSell;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Lilly
+{
+    [StaticConstructorOnStartup]
+    public static class RWAutoSell_Patch
+    {
+        public static string harmonyId = "Lilly.RWAutoSell";
+        public static Harmony harmony;
+        public static bool onDebug = false;
+
+        static RWAutoSell_Patch() 
+        {
+            if (harmony == null)
+            {
+                MyLog.Warning($"Patch ST");
+                try
+                {
+                    harmony = new Harmony(harmonyId);
+                    harmony.PatchAll();
+                    MyLog.Warning($"{harmonyId} Patch Succ");
+                }
+                catch (System.Exception e)
+                {
+                    MyLog.Error($"{harmonyId} Patch Fail");
+                    MyLog.Error(e.ToString());
+                    MyLog.Error($"{harmonyId} Patch Fail");
+                }
+                MyLog.Warning($"Patch ED");
+            }
+        }
+
+        public static List<IRule> ruleList = new List<IRule>();
+
+        public static Map tmpmap;
+
+        [HarmonyPatch(typeof(SavedGameLoaderNow), nameof(SavedGameLoaderNow.LoadGameFromSaveFileNow))]
+        public static class Patch_LoadGame
+        {
+            [HarmonyPostfix]
+            public static void Patch()
+            {
+                MyLog.Warning($"LoadGameFromSaveFileNow ST", print: onDebug);
+
+                var aSMapComp = ASMapComp.GetSingleton(Find.CurrentMap);
+                if (aSMapComp == null)
+                {
+                    MyLog.Error($"LoadGameFromSaveFileNow ASMapComp NULL");
+                    return;
+                }
+                ruleList.Clear();
+                foreach (var rule in aSMapComp.EnumerateRules())
+                {
+                    ruleList.Add(rule.DeepCopy());
+                }
+                MyLog.Warning($"LoadGameFromSaveFileNow ED {aSMapComp.Rules.Count}", print: onDebug);
+            }
+        }
+
+        [HarmonyPatch(typeof(ASMapComp), nameof(ASMapComp.MapGenerated))]
+        public static class Patch_MapGenerated
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ASMapComp __instance)
+            {
+                MyLog.Warning($"MapGenerated ST", print: onDebug);
+
+                try
+                {
+                    MyLog.Warning($"map {__instance?.map}", print: onDebug);
+                    MyLog.Warning($"Find.Maps.Count {Find.Maps?.Count} ", print: onDebug);
+
+                    var aSMapComp = __instance;
+                    if (aSMapComp == null)
+                    {
+                        MyLog.Error($"AddMap ASMapComp NULL");
+                        return;
+                    }
+                    if (Find.Maps.Count == 1)
+                    {
+                        tmpmap = __instance.map;
+                        foreach (var rule in ruleList)
+                        {
+                            aSMapComp.Add(rule.DeepCopy());
+                        }
+                    }
+                    else if (Find.Maps.Count > 1)
+                    {
+                        foreach (var rule in ASMapComp.GetSingleton(Find.Maps[Find.Maps.Count - 2]).EnumerateRules())
+                        {
+                            aSMapComp.Add(rule.DeepCopy());
+                        }
+                    }
+                    MyLog.Warning($"aSMapComp.Rules.Count {aSMapComp.Rules.Count}", print: onDebug);
+                }
+                catch (Exception e)
+                {
+                    MyLog.Error(e.ToString());
+                }
+                MyLog.Warning($"MapGenerated ED", print: onDebug);
+            }
+        }
+
+        [HarmonyPatch(typeof(ASMapComp), nameof(ASMapComp.MapRemoved))]
+        public static class MapRemoved
+        {
+            [HarmonyPrefix]
+            public static void Postfix(ASMapComp __instance)
+            {
+                try
+                {
+                    MyLog.Warning($"MapRemoved ST", print: onDebug);
+                    if (Find.Maps?.Count == 0)
+                    {
+                        ruleList.Clear();
+                        foreach (var rule in __instance.EnumerateRules())
+                        {
+                            ruleList.Add(rule.DeepCopy());
+                        }
+                    }
+                    MyLog.Warning($"__instance.Rules.Count {__instance?.Rules?.Count}", print: onDebug);
+                }
+                catch (Exception e)
+                {
+                    MyLog.Error(e.ToString());
+                }
+                MyLog.Warning($"MapRemoved ED", print: onDebug);
+            }
+        }
+
+        //[HarmonyPatch(typeof(Game), nameof(Game.DeinitAndRemoveMap))]// 정상이지만 중복이라 제거
+        public static class Patch_RemoveMap
+        {
+            [HarmonyPrefix]
+            //[HarmonyPostfix]
+            public static void Patch(Map map)
+            {
+                MyLog.Warning($"DeinitAndRemoveMap ST", print: onDebug);
+                try
+                {
+                    MyLog.Warning($"map {map}", print: onDebug);
+
+                    MyLog.Warning($"Find.Maps.Count {Find.Maps?.Count}", print: onDebug);
+
+                    if (Find.Maps?.Count > 0)
+                    {
+                        var aSMapComp = ASMapComp.GetSingleton(Find.CurrentMap);
+                        if (aSMapComp == null)
+                        {
+                            MyLog.Error($"DeinitAndRemoveMap ASMapComp NULL");
+                            return;
+                        }
+                        MyLog.Warning($"aSMapComp.Rules.Count  {aSMapComp.Rules.Count}", print: onDebug);
+                        ruleList.Clear();
+                        foreach (var rule in aSMapComp.EnumerateRules())
+                        {
+                            ruleList.Add(rule.DeepCopy());
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    MyLog.Error(e.ToString());
+                }
+                MyLog.Warning($"DeinitAndRemoveMap ED", print: onDebug);
+            }
+        }
+
+    }
+}
